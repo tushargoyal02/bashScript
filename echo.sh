cf62ee82d776d1a3deb292e59cf508ddd2bb476e
Tushar Goyal <tushargoyal19tj@gmail.com>
2020-03-12 14:21:01 +0530

    Echo & comment

---
+++ echo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# Echo is used to print whatever you have typed here
+    # It will automatically let you with new line
+echo "hello this is the first program"
+echo "hello to second line"
+
+
+
+
+
+# To get rid of new line use "-n": It will tell to forgot about new line
+echo -n "1st line "
+echo "Second line"
+
+
+# To allow special character like "\n" in echo [USE -e ]
+
+echo "\nSpecial character"
+
+echo -e "\n special character series"
+
+
+# To run it in the termial type
+    # bash file.sh      [File extension should be sh]
+
+
