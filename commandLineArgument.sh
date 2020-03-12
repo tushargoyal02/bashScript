390840067db7b6383386bb1b810d08d57aa87767
Tushar Goyal <tushargoyal19tj@gmail.com>
2020-03-13 00:48:48 +0530

    get command line arguments

---
+++ commandLineArgument.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# To determine the total arguments
+    # To get the value of the arguments
+
+args=$#
+    var=$1
+    var2=$2
+
+echo "Arguments value are like:"
+echo $var $var2
---
+++ tablePrint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo -n "Put the number for table : "
+read var
+
+count=1
+
+# FOLLOW THIS SYNTAX
+
+if [ $var -le 0 ];
+then
+    break
+else
+    while [ $count -le 10 ] ; do
+
+    echo -n "Table are :"
+    ((output=$var*$count))
+    
+    echo "Table for $var X $count = "$output
+
+    let count=count+1
+    
+    done
+
+fi
+
+
